48a7ae69ef8debf59d95809c1f662d2d3896b748
Poe <tshepok13@gmail.com>
2020-04-01 16:47:21 +0000

    more ground work: add article models and methods

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,15 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+  def new
+  end
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :text, :author))
+    @article.save
+    redirect_to @article
+  end
+end
