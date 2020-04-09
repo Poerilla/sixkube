1eefe18d2f628cd5ba2dcdacd3ac339516839283
Poe <tshepok13@gmail.com>
2020-04-09 20:29:03 +0000

    upgrading versions and Dockerfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,3 @@
 Rails.application.routes.draw do
-  get 'home/index'
-  resources :sessions, only: [:new, :create, :destroy]
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :users
-  get 'welcome/index'
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'  
-  resources :articles do
-    resources :comments
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
 end
